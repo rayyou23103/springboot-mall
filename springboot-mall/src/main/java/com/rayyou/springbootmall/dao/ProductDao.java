5be9997addb9559f6b0a15a46fa7d0ce2dc61d47
youshixuan <k9102188@gmail.com>
2025-01-02 23:02:40 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/rayyou/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/rayyou/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.rayyou.springbootmall.dao;
 import com.rayyou.springbootmall.dto.ProductRequest;
 import com.rayyou.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/rayyou/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/rayyou/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.rayyou.springbootmall.service;
 import com.rayyou.springbootmall.dto.ProductRequest;
 import com.rayyou.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
 
