64849e3e820a9c0fc265ccecf107f73d825aae8f
youshixuan <k9102188@gmail.com>
2025-01-11 00:43:12 +0800

    訂單功能 - 檢查 user 是否存在、扣除商品庫存

--- springboot-mall/src/main/java/com/rayyou/springbootmall/service/impl/OrderServiceImpl.java
+++ springboot-mall/src/main/java/com/rayyou/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,27 +2,39 @@ package com.rayyou.springbootmall.service.impl;
 
 import com.rayyou.springbootmall.dao.OrderDao;
 import com.rayyou.springbootmall.dao.ProductDao;
+import com.rayyou.springbootmall.dao.UserDao;
 import com.rayyou.springbootmall.dto.BuyItem;
 import com.rayyou.springbootmall.dto.CreateOrderRequest;
 import com.rayyou.springbootmall.model.Order;
 import com.rayyou.springbootmall.model.OrderItem;
 import com.rayyou.springbootmall.model.Product;
+import com.rayyou.springbootmall.model.User;
 import com.rayyou.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Component
 public class OrderServiceImpl implements OrderService {
+
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -37,28 +49,48 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // 檢查user 是否存在
+        User user = userDao.getUserById(userId);
+        if (user == null) {
+            log.warn("該 user{} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmout = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         // 計算訂單金額
-        for (BuyItem buyItem : createOrderRequest.getBuyItemList()){
-        Product product = productDao.getProductById(buyItem.getProductId());
+        for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
+            Product product = productDao.getProductById(buyItem.getProductId());
 
-        int amount = buyItem.getQuantity() * product.getPrice();
-        totalAmout = totalAmout + amount;
+            // 檢查product 是否存在、庫存是否足夠
+            if (product == null) {
+                log.warn("商品 {} 不存在", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } else if (product.getStock() < buyItem.getQuantity()) {
+                log.warn("商品 {} 庫存數量不足，無法購買，剩餘庫存 {}，欲購買數量{}",
+                        buyItem.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            // 扣除商品
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
+
+            int amount = buyItem.getQuantity() * product.getPrice();
+            totalAmout = totalAmout + amount;
 
 
-        // 轉換 BuyItem to OrderItem
-        OrderItem orderItem = new OrderItem();
-        orderItem.setProductId(buyItem.getProductId());
-        orderItem.setQuantity(buyItem.getQuantity());
-        orderItem.setAmount(amount);
+            // 轉換 BuyItem to OrderItem
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProductId(buyItem.getProductId());
+            orderItem.setQuantity(buyItem.getQuantity());
+            orderItem.setAmount(amount);
 
-        orderItemList.add(orderItem);
+            orderItemList.add(orderItem);
         }
 
         // 創建訂單
-        Integer orderId = orderDao.createOrder(userId,totalAmout);
+        Integer orderId = orderDao.createOrder(userId, totalAmout);
 
         orderDao.createOrderItems(orderId, orderItemList);
         return orderId;
